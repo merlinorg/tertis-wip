3989ad21405a67ba1d5c3c413a085f6b6e675aae
merlin <merlin@merlin.org>
2025-02-16 11:55:49 -0500

    Copy more things

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,23 +59,14 @@ android {
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
     scalroid {
         scala.zincVersion = "1.8.0"
     }
-//    scalroid {
-    //scala.zincVersion = '1.8.0'
-//    }
-
-//    sourceSets {
-//        main {
-//
-//        }
-//    }
 }
 
 tasks.configureEach {
